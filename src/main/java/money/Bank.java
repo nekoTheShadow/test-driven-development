2265b3e6a8f949b1cbdaee5a4826d5960fda1881
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2019-01-30 00:39:23 +0900

    pp.93-94

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -3,8 +3,7 @@ package money;
 public class Bank {
     public Money reduce(Expression source, String to) {
         var sum = (Sum) source;
-        int amount = sum.augend.amount + sum.addend.amount;
-        return new Money(amount, to);
+        return sum.reduce(to);
     }
 
 }
