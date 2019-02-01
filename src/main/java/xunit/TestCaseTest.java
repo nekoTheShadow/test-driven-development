84cf373ef180c427726ea96b8c88e586517e8904
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2019-02-01 23:41:36 +0900

    pp.180-182

--- src/main/java/xunit/TestCaseTest.java
+++ src/main/java/xunit/TestCaseTest.java
@@ -4,30 +4,32 @@ public class TestCaseTest extends TestCase {
     public TestCaseTest(String name) {
         super(name);
     }
+    
+    private TestResult result;
+    
+    public void setUp() {
+        result = new TestResult();
+    }
 
     public void testTemplateMethod() {
         var test = new WasRun("testMethod");
-        var result = new TestResult();
         test.run(result);
         XUnitRuntimeException.assertion(() -> test.log.equals("setUp testMethod tearDown "));
     }
     
     public void testResult() {
         var test = new WasRun("testMethod");
-        var result = new TestResult();
         test.run(result);
         XUnitRuntimeException.assertion(() -> result.summary().equals("1 run, 0 failed"));
     }
     
     public void testFailedResult() {
         var test = new WasRun("testBrokenMethod");
-        var result = new TestResult();
         test.run(result);
         XUnitRuntimeException.assertion(() -> result.summary().equals("1 run, 1 failed"));
     }
     
     public void testFailedResultFormatting() {
-        var result = new TestResult();
         result.testStarted();
         result.testFailed();
         XUnitRuntimeException.assertion(() -> result.summary().equals("1 run, 1 failed"));
@@ -37,7 +39,6 @@ public class TestCaseTest extends TestCase {
         var suite = new TestSuite();
         suite.add(new WasRun("testMethod"));
         suite.add(new WasRun("testBrokenMethod"));
-        var result = new TestResult();
         suite.run(result);
         XUnitRuntimeException.assertion(() -> result.summary().equals("2 run, 1 failed"));
     }
